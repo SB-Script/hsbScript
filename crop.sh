46601d0384cd85f4c82ed901dc33ad98255aa578
Sam Blamer <152201467+b-lamer@users.noreply.github.com>
2025-06-13 21:13:11 -0500

    Create crop.sh

---
+++ crop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while true; do
+        xdotool mousedown 1
+        xdotool keydown s; sleep 4; xdotool keyup s
+        xdotool keydown a; sleep 0.3; xdotool keyup a
+    for i in {1..40}; do
+        xdotool mouseup 1
+        xdotool mousedown 1
+        xdotool keydown w; sleep 5.8; xdotool keyup w
+        xdotool keydown a; sleep 0.3; xdotool keyup a
+        xdotool keydown s; sleep 5.8; xdotool keyup s
+        xdotool keydown a; sleep 0.3; xdotool keyup a
+    done
+    xdotool keydown d; sleep 6; xdotool keyup d
+done
